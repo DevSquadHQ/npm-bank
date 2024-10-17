d7c1bfe3fb501e93c94ed6a5a9dfc2e60cf026a5
neginz <negin.z566@gmail.com>
2024-10-18 01:54:17 +0330

    [feat] Mui theme for general style

---
+++ src/theme.js
@@ -0,0 +1,33 @@
+
+
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Vazirmatn", "Roboto", "Helvetica", "Arial", sans-serif',
+  },
+  palette: {
+    background: {
+      default: '#111928', // App background color
+    },
+    primary: {
+      main: '#1C64F2', // Main color for buttons
+    },
+    text: {
+      primary: '#FFFFFF', // Main text color
+      secondary: '#9CA3AF', // Lighter text color
+    },
+    custom: {
+      authBox: '#1F2A37', // Box around the auth form
+      inputBackground: '#374151', // Lighter part like input background
+    },
+    success: {
+      main: '#014737', // Success color
+    },
+    error: {
+      main: '#771D1D', // Warning or error color
+    },
+  }
+});
+
+export default theme;
