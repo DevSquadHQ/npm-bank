f8a7b857b9a01157dff9eaab966c43eca74b9873
parvin <parvinnoori0@gmail.com>
2024-10-17 15:07:35 +0330

    Inhance UI: button component style in responsive

--- src/pages/signup/SignUpForm.jsx
+++ src/pages/signup/SignUpForm.jsx
@@ -43,25 +43,31 @@ function FirstForm(props) {
   const { handleChange, handleSubmit } = props;
 
   return (
-    <form action="" className="space-y-5" onSubmit={handleSubmit}>
-      <InputGroup
-        label="نام"
-        id="name"
-        placeholder="لطفا نام خود را وارد کنید"
-        onChange={handleChange}
-      />
-      <InputGroup
-        label="نام خانوادگی"
-        id="lastName"
-        placeholder="لطفا نام خانوادگی خود را وارد کنید"
-        onChange={handleChange}
-      />
-      <InputGroup
-        label="کد ملی"
-        id="nationalCode"
-        placeholder="لطفا کد ملی خود را وارد کنید"
-        onChange={handleChange}
-      />
+    <form
+      action=""
+      className="space-y-5 h-full flex flex-col"
+      onSubmit={handleSubmit}
+    >
+      <div className="space-y-5 grow sm:grow-0">
+        <InputGroup
+          label="نام"
+          id="name"
+          placeholder="لطفا نام خود را وارد کنید"
+          onChange={handleChange}
+        />
+        <InputGroup
+          label="نام خانوادگی"
+          id="lastName"
+          placeholder="لطفا نام خانوادگی خود را وارد کنید"
+          onChange={handleChange}
+        />
+        <InputGroup
+          label="کد ملی"
+          id="nationalCode"
+          placeholder="لطفا کد ملی خود را وارد کنید"
+          onChange={handleChange}
+        />
+      </div>
       <Button label="ادامه" />
       <span className="text-[#6B7280] block">
         حساب کاربری دارید ؟{" "}
@@ -76,28 +82,34 @@ function SecondForm(props) {
   const { handleChange, handleSubmit, handleBack } = props;
 
   return (
-    <form action="" className="space-y-5" onSubmit={handleSubmit}>
-      <InputGroup
-        label="تاریخ تولد"
-        id="birthdate"
-        placeholder="انتخاب تاریخ"
-        onChange={handleChange}
-        type="date"
-      />
-      <InputGroup
-        label="شماره موبایل"
-        id="PhoneNumber"
-        placeholder="مثال 09121212121"
-        onChange={handleChange}
-        type="tel"
-      />
-      <InputGroup
-        label="ایمیل"
-        id="email"
-        placeholder="لطفا ایمیل خود را وارد کنید"
-        onChange={handleChange}
-        type="email"
-      />
+    <form
+      action=""
+      className="space-y-5 h-full flex flex-col"
+      onSubmit={handleSubmit}
+    >
+      <div className="space-y-5 grow">
+        <InputGroup
+          label="تاریخ تولد"
+          id="birthdate"
+          placeholder="انتخاب تاریخ"  
+          onChange={handleChange}
+          type="date"
+        />
+        <InputGroup
+          label="شماره موبایل"
+          id="PhoneNumber"
+          placeholder="مثال 09121212121"
+          onChange={handleChange}
+          type="tel"
+        />
+        <InputGroup
+          label="ایمیل"
+          id="email"
+          placeholder="لطفا ایمیل خود را وارد کنید"
+          onChange={handleChange}
+          type="email"
+        />
+      </div>
       <Button label="ثبت نام" />
       <Button
         variant="outlined"
