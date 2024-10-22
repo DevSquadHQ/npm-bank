bdcf7eb6a4e2b6cccdfcb2b6bfb4b208a54e402e
parvin <parvinnoori.dev@gmail.com>
2024-10-22 19:38:15 +0330

    feat: form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,17 @@
+import { Card, Typography } from "antd";
+const { Text } = Typography;
+import "./form.css"
+
 export default function Form(props) {
   const { title, children } = props;
   return (
-    <div className="text-white w-[384px] flex flex-col space-y-6 bg-form p-[32px] rounded-[8px] sm:grow-0 grow">
-      <span className="text-[20px]">{title}</span>
+    <Card className="form-container">
+      <Text className="title">{title}</Text>
       {children}
-    </div>
+    </Card>
+    // <div className="text-white w-[384px] flex flex-col space-y-6 bg-form p-[32px] rounded-[8px] sm:grow-0 grow">
+    //   <span className="text-[20px]">{title}</span>
+    //   {children}
+    // </div>
   );
 }
