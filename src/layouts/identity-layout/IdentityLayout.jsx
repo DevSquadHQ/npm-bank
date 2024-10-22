0089c791bedd97f5df25e2bb68ac1e42a424c4ad
parvin <parvinnoori.dev@gmail.com>
2024-10-22 19:12:20 +0330

    feat:layout

--- src/layouts/identity-layout/IdentityLayout.jsx
+++ src/layouts/identity-layout/IdentityLayout.jsx
@@ -15,19 +15,19 @@ export default function IdentityLayout() {
     textAlign: "center",
   };
   return (
-    <Layout className="identity-layout">
-      <Content className="flex items-center">
-        <Row className="flex-col">
-          <Title className="main-title">اینترنت بانک من</Title>
-          <Outlet />
-        </Row>
-      </Content>
-    </Layout>
-    // <div className="h-svh w-svw bg-Navy-blue flex sm:items-center sm:justify-center sm:p-0 py-5">
-    //   <div className="flex flex-col sm:space-y-14 space-y-5 mx-auto">
-    //     <span className="text-white text-[32px] text-center">اینترنت بانک من</span>
-    //     <Outlet />
-    //   </div>
-    // </div>
+    // <Layout className="identity-layout">
+    //   <Content className="flex items-center">
+    //     <Row className="flex-col">
+    //       <Title className="main-title">اینترنت بانک من</Title>
+    //       <Outlet />
+    //     </Row>
+    //   </Content>
+    // </Layout>
+    <div className="h-svh w-svw bg-Navy-blue flex sm:items-center sm:justify-center sm:p-0 py-5">
+      <div className="flex flex-col sm:space-y-14 space-y-5 mx-auto">
+        <span className="text-white text-[32px] text-center">اینترنت بانک من</span>
+        <Outlet />
+      </div>
+    </div>
   );
 }
