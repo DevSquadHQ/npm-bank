7c3e41a31c9f7abc9b70ede998046fa426050d48
parvin <parvinnoori.dev@gmail.com>
2024-10-22 19:25:02 +0330

    remove extra file

--- src/layouts/identity-layout/IdentityLayout.jsx
+++ src/layouts/identity-layout/IdentityLayout.jsx
@@ -1,31 +1,13 @@
-import { Layout, Row, theme, Typography } from "antd";
 import React from "react";
 import { Outlet } from "react-router-dom";
-const { Content } = Layout;
-const { Title } = Typography;
-const { useToken } = theme;
-import "./identity-layout.css";
 
 export default function IdentityLayout() {
-  const { token } = useToken();
-
-  const TitleStyle = {
-    color: "white",
-    fontSize: "32px",
-    textAlign: "center",
-  };
   return (
-    // <Layout className="identity-layout">
-    //   <Content className="flex items-center">
-    //     <Row className="flex-col">
-    //       <Title className="main-title">اینترنت بانک من</Title>
-    //       <Outlet />
-    //     </Row>
-    //   </Content>
-    // </Layout>
     <div className="h-svh w-svw bg-Navy-blue flex sm:items-center sm:justify-center sm:p-0 py-5">
       <div className="flex flex-col sm:space-y-14 space-y-5 mx-auto">
-        <span className="text-white text-[32px] text-center">اینترنت بانک من</span>
+        <span className="text-white text-[32px] text-center">
+          اینترنت بانک من
+        </span>
         <Outlet />
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
-import { ConfigProvider } from "antd";
-
-const theme = {
-  token: {
-    NavyBlue: "#111928",
-    form: "#1F2A37",
-    input: "#374151",
-    button: "#1C64F2",
-    fontFamily:'vazir'
-  },
-};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ConfigProvider theme={theme} direction="rtl">
-      <RouterProvider router={router} />
-    </ConfigProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
