107dbca51b4d4348145cfa4b8e8f9b177f7f1694
Parvin Noori <parvinnoori0@gmail.com>
2024-10-27 11:16:21 +0330

    refactor folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,56 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import MainLayout from "./layouts/main-layout/MainLayout.jsx";
-import Home from "./pages/main-pages/Home.jsx";
-import Report from "./pages/main-pages/Report.jsx";
-import MoneyTransfer from "./pages/main-pages/MoneyTransfer.jsx";
-import AccountSuspension from "./pages/main-pages/AccountSuspension.jsx";
-import ChangePassword from "./pages/main-pages/ChangePassword.jsx";
-import AccountList from "./pages/main-pages/AccountList.jsx";
-import MyAccount from "./pages/main-pages/MyAccount.jsx";
-import CreateAccount from "./pages/main-pages/CreateAccount.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        element: <Home />,
-        index: true,
-      },
-      {
-        path: "/report",
-        element: <Report />,
-      },
-      {
-        path: "/moneyTransfer",
-        element: <MoneyTransfer />,
-      },
-      {
-        path: "/accountSuspension",
-        element: <AccountSuspension />,
-      },
-      {
-        path: "/changePassword",
-        element: <ChangePassword />,
-      },
-      {
-        path: "/accoutList",
-        element: <AccountList />,
-      },
-      {
-        path: "/myAccount",
-        element: <MyAccount />,
-      },
-      {
-        path: "/createAccount",
-        element: <CreateAccount />,
-      },
-    ],
-  },
-]);
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
---
+++ src/router.jsx
@@ -0,0 +1,53 @@
+
+import {  createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layouts/main-layout//MainLayout.jsx";
+import Home from "./pages/main-pages/Home/Home.jsx";
+import Report from "./pages/main-pages/Report/Report.jsx";
+import MoneyTransfer from "./pages/main-pages/Money-transfer/MoneyTransfer.jsx";
+import AccountSuspension from "./pages/main-pages/Account-suspension/AccountSuspension.jsx";
+import ChangePassword from "./pages/main-pages/Change-password/ChangePassword.jsx";
+import AccountList from "./pages/main-pages/Account-list/AccountList.jsx";
+import MyAccount from "./pages/main-pages/My-account/MyAccount.jsx";
+import CreateAccount from "./pages/main-pages/Create-account/CreateAccount.jsx";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          element: <Home />,
+          index: true,
+        },
+        {
+          path: "/report",
+          element: <Report />,
+        },
+        {
+          path: "/moneyTransfer",
+          element: <MoneyTransfer />,
+        },
+        {
+          path: "/accountSuspension",
+          element: <AccountSuspension />,
+        },
+        {
+          path: "/changePassword",
+          element: <ChangePassword />,
+        },
+        {
+          path: "/accoutList",
+          element: <AccountList />,
+        },
+        {
+          path: "/myAccount",
+          element: <MyAccount />,
+        },
+        {
+          path: "/createAccount",
+          element: <CreateAccount />,
+        },
+      ],
+    },
+  ]);
+  
