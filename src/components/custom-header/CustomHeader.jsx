820f85581abf5cce7060057cd504f2b05cc9a7c4
neginz <negin.z566@gmail.com>
2024-10-28 16:22:35 +0330

    [feat] add Create Acount form

--- src/components/custom-header/CustomHeader.jsx
+++ src/components/custom-header/CustomHeader.jsx
@@ -2,6 +2,7 @@ import CustomPopover from "../custom-popover/CustomPopover";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import { Button, Layout } from "antd";
 import "./custom-header.css";
+import { Link } from "react-router-dom";
 
 const { Header } = Layout;
 
@@ -9,13 +10,20 @@ export default function CustomHeader(props) {
   const { toggle, collapsed } = props;
   return (
     <Header className="custom-header">
-      <Button type="text" size="large"  onClick={toggle} className="collapsed-button">
+      <Button
+        type="text"
+        size="large"
+        onClick={toggle}
+        className="collapsed-button"
+      >
         {collapsed ? <MenuFoldOutlined /> : <MenuUnfoldOutlined />}
       </Button>
-      <Button type="primary" variant="link">
-        ساخت حساب جدید
-        <span>+</span>
-      </Button>
+      <Link to={"/createAccount"}>
+        <Button type="primary" variant="link">
+          ساخت حساب جدید
+          <span>+</span>
+        </Button>
+      </Link>
       <CustomPopover />
     </Header>
   );
