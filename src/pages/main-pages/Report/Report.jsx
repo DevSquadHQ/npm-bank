2115189fb25c006f066a98001c09f43c506ca571
parvin-noori <parvinnoori.dev@gmail.com>
2024-10-29 17:53:08 +0330

    [feat]:report page

--- src/pages/main-pages/Report/Report.jsx
+++ src/pages/main-pages/Report/Report.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { Typography } from "antd";
+const { Text } = Typography;
 
 export default function Report() {
   return (
-    <div>Report</div>
-  )
+    <>
+      <Text className="card-title">گزارش مالی</Text>
+    </>
+  );
 }
