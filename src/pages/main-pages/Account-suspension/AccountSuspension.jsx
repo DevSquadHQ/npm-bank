832c4a8d3f613f1ae4879228522529fe09c258f2
parvin-noori <parvinnoori.dev@gmail.com>
2024-10-30 21:17:19 +0330

    [feat]:account suspenstion

--- src/pages/main-pages/Account-suspension/AccountSuspension.jsx
+++ src/pages/main-pages/Account-suspension/AccountSuspension.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { Button, Flex, Form } from "antd";
+import CustomTable from "../../../components/custom-table/CustomTable";
+import { useState } from "react";
+
+const tableData = [
+  { id: "1", label: "شماره سپرده", amount: "1111111111" },
+  { id: "2", label: "شماره کارت", amount: "1111111111111" },
+  { id: "3", label: "نوع حساب", amount: "جاری" },
+];
 
 export default function AccountSuspension() {
+  const [form] = Form.useForm();
+  const [isBlock, setIsBlock] = useState(false);
+  const accountId = "2";
+
+  const onFinish = () => {
+    console.log("Account ID:", accountId);
+  };
+
   return (
-    <div>AccountSuspension</div>
-  )
+    <Flex vertical gap="large">
+      <CustomTable tableData={tableData} />
+      <Form form={form} action="" onFinish={onFinish}>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+            {isBlock ? "رفع مسدودی" : "مسدود کردن"}
+          </Button>
+        </Form.Item>
+      </Form>
+    </Flex>
+  );
 }
