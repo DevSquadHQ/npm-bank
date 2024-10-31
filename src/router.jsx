3ec314385d9136e9a94d9a75fb19828962167915
neginz <negin.z566@gmail.com>
2024-10-31 22:29:25 +0330

    [chore] fix a tiny typo

--- src/router.jsx
+++ src/router.jsx
@@ -39,7 +39,7 @@ export const router = createBrowserRouter([
         element: <ChangePassword />,
       },
       {
-        path: "/accoutList",
+        path: "/accountList",
         element: <AccountList />,
       },
       {
