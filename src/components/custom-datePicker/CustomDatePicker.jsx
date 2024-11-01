88c46566ac120d090ebe9673bd3f4014da964b45
parvin-noori <parvinnoori0@gmail.com>
2024-11-01 13:53:58 +0330

    [fix bug]:custom date picker

--- src/components/custom-datePicker/CustomDatePicker.jsx
+++ src/components/custom-datePicker/CustomDatePicker.jsx
@@ -3,27 +3,25 @@ import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
 import InputIcon from "react-multi-date-picker/components/input_icon";
 import "../../layouts/identity-layout/identity-layout.css";
-
-
-import { useState } from "react";
+import { CalendarOutlined } from "@ant-design/icons";
+import { Flex } from "antd";
 
 export default function CustomDatePicker({
   isRange = false,
   onChange,
   initialValue,
-  maxDate
+  maxDate,
 }) {
-
-  const [value, setValue] = useState(isRange ? initialValue : maxDate);
+  const value = isRange ? initialValue : maxDate;
 
   return (
     <DatePicker
       containerClassName="datePicker-container"
-      inputClass="custom-datePicker"
+      render={<CustomInput />}
       placeholder={isRange ? "انتخاب محدوده تاریخ" : "انتخاب تاریخ"}
       value={value}
       onChange={(value) => onChange(value)}
-      render={<InputIcon />}
+      // render={<InputIcon />}
       maxDate={!isRange ? maxDate : undefined}
       range={isRange}
       calendar={persian}
@@ -31,5 +29,18 @@ export default function CustomDatePicker({
       format={"YYYY/MM/DD"}
       calendarPosition="bottom-left"
     />
+  );
+}
+function CustomInput({ onFocus, value, onChange }) {
+  return (
+    <Flex items="center">
+      <input
+        className="custom-datePicker"
+        onFocus={onFocus}
+        value={value}
+        onChange={onChange}
+      />
+      <CalendarOutlined />
+    </Flex>
   );
 }
