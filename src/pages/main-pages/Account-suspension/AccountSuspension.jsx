fb6144fe36139b4b9bd886c97f0d6d15ad635f75
Parvin Noori <parvinnoori0@gmail.com>
2024-11-02 10:22:55 +0330

    [chore]:update acc suspension

--- src/pages/main-pages/Account-suspension/AccountSuspension.jsx
+++ src/pages/main-pages/Account-suspension/AccountSuspension.jsx
@@ -1,11 +1,12 @@
 import { Button, Flex, Form } from "antd";
 import CustomTable from "../../../components/custom-table/CustomTable";
+import { accountInfoColumns } from "../../../components/custom-table/tableColumnsConfig";
 import { useState } from "react";
 
 const tableData = [
-  { id: "1", label: "شماره سپرده", value: "1111111111" },
-  { id: "2", label: "شماره کارت", value: "1111111111111" },
-  { id: "3", label: "نوع حساب", value: "جاری" },
+  { key: "1", label: "شماره سپرده", value: "1111111111" },
+  { key: "2", label: "شماره کارت", value: "1111111111111" },
+  { key: "3", label: "نوع حساب", value: "جاری" },
 ];
 
 export default function AccountSuspension() {
@@ -19,7 +20,9 @@ export default function AccountSuspension() {
 
   return (
     <Flex vertical gap="large">
-      <CustomTable tableData={tableData} />
+      <CustomTable tableData={tableData} columnsConfig={accountInfoColumns}
+        selectable={false}
+        pagination={false}/>
       <Form form={form} action="" onFinish={onFinish}>
         <Form.Item>
           <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
