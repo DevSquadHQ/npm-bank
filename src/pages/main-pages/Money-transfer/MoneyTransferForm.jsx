527b258159e2beef79eda54c4ad176695b311097
Mahdi <mahdi1nap@gmail.com>
2024-11-03 21:35:54 +0330

    change fee amount input's style

--- src/pages/main-pages/Money-transfer/MoneyTransferForm.jsx
+++ src/pages/main-pages/Money-transfer/MoneyTransferForm.jsx
@@ -7,16 +7,24 @@ import persian_fa from "react-date-object/locales/persian_fa";
 import Inputs4Digit from "./Inputs4Digit";
 import OtpModal from "./OtpModal.jsx";
 import { getToday } from "../../../utils/indentityUtils.js";
+import Num2persian from 'num2persian';
 
 export default function MoneyTransferForm() {
   const [form] = Form.useForm();
   const [open, setOpen] = useState(false);
+  const [fee, setFee] = useState(undefined); 
 
   const today = getToday();
 
   const onFinish = (values) => {
     console.log("Success", values);
     setOpen(true);
+  };
+
+  const handleFeeChange = (value) => {
+    const parsedValue = parseInt(value, 10);
+    setFee(isNaN(parsedValue) ? undefined : parsedValue); 
+    console.log("Fee amount:", parsedValue); 
   };
 
   return (
@@ -27,7 +35,7 @@ export default function MoneyTransferForm() {
         onFinish={onFinish}
         requiredMark={false}
         validateMessages={validateMessages}
-        initialValues={{ expirationDate: today }} // Set initial values here
+        initialValues={{ expirationDate: today.format("YYYY/MM/DD") }}
         labelCol={{
           span: 24,
         }}
@@ -42,21 +50,32 @@ export default function MoneyTransferForm() {
           <Inputs4Digit />
         </Form.Item>
         <Form.Item
-          label="مبلغ (تومان)"
+          label="مبلغ"
           name="fee"
+          tooltip="حداقل ۱۰ هزار تومان"
           rules={[{ required: true }]}
           validateTrigger="onChange"
           validateFirst
         >
           <InputNumber
-            style={{ width: "100%" }}
+            addonAfter="ریال"
+            style={{ width: "100%",backgroundColor: "inherit" ,padding:0}
+          }
             className="inputStyle"
             placeholder="مبلغ مورد نیاز را وارد کنید"
             min={10000}
+            value={fee} 
+            onChange={handleFeeChange} 
             formatter={(value) =>
-              value ? `${value} `.replace(/\B(?=(\d{3})+(?!\d))/g, ",") : ""
+              value ? `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",") : ""
             }
+            parser={(value) => value?.replace(/\$\s?|(,*)/g, '')}
           />
+          {fee !== undefined && fee >= 10000 && (
+            <div style={{ marginTop: "8px", color: "#888" }}>
+               {Num2persian(fee)} تومان
+            </div>
+          )}
         </Form.Item>
         <Form.Item
           label="CVV2"
